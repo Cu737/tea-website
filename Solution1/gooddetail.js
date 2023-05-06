deada45f59935ff196cd232a7fa57c943d323db7
雨后天晴 <oncwnuALQzkrUxUWw7Va9cRzwQVo@git.weixin.qq.com>
2023-05-06 23:59:15 +0800

    购买页面初稿

---
+++ Solution1/gooddetail.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+let str;
+function add(){
+    str = document.getElementById("text");
+    str.value = Number(str.value) + 1;
+}
+function sub() {
+    str = document.getElementById("text");
+    str.value = str.value -1;
+}
