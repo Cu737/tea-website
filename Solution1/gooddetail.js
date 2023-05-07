2e318eb59a9e2b81d4c64e98193f38b5932c5cf6
雨后天晴 <oncwnuALQzkrUxUWw7Va9cRzwQVo@git.weixin.qq.com>
2023-05-07 19:20:55 +0800

    商品购买页面信息版

--- Solution1/gooddetail.js
+++ Solution1/gooddetail.js
@@ -1,6 +1,9 @@
 // JavaScript source code
 let str;
 let str2;
+let name;
+let tele;
+let addrss;
 function add(){
     str = document.getElementById("text");
     str.value = Number(str.value) + 1;
@@ -12,4 +15,12 @@ function sub() {
     str.value = str.value - 1;
     str2 = document.getElementById("cost");
     str2.value = Number(str.value) *120;
+}
+function check() {
+    str = document.getElementById("text");
+    str2 = document.getElementById("cost");
+    name = document.getElementById("name");
+    tele = document.getElementById("telenum");
+    address = document.getElementById("address");
+    //如果需要传送信息给后端的话可以直接用啦~，str是物品数目，str2是商品总价
 }
