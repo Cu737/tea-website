43260091dd63ebb193e1f75cb9c589269b54d4ff
雨后天晴 <oncwnuALQzkrUxUWw7Va9cRzwQVo@git.weixin.qq.com>
2023-05-07 17:58:10 +0800

    商品交易界面完整版

--- Solution1/gooddetail.js
+++ Solution1/gooddetail.js
@@ -1,10 +1,15 @@
 // JavaScript source code
 let str;
+let str2;
 function add(){
     str = document.getElementById("text");
     str.value = Number(str.value) + 1;
+    str2 = document.getElementById("cost");
+    str2.value = Number(str.value)*120;
 }
 function sub() {
     str = document.getElementById("text");
-    str.value = str.value -1;
+    str.value = str.value - 1;
+    str2 = document.getElementById("cost");
+    str2.value = Number(str.value) *120;
 }
