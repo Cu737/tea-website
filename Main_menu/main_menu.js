4ccc2e73c7919dc1a813006ca9ab55fb2c6f9e90
liuchanghan <2594651085@qq.com>
2023-05-10 17:31:36 +0800

    茶的种类部分的翻转效果

--- Main_menu/main_menu.js
+++ Main_menu/main_menu.js
@@ -3,7 +3,7 @@ Vue.createApp({
         return {
             text: "hello",
             message: "helloo",
-            judge_switch:true,
+            judge_switch:[true,true,true,true,true,true],
             facade_item:[
                 {src_img:"../image/tea_kind2.jpg",img_text:"红茶"},
                 {src_img:"../image/tea_kind1.jpg",img_text:"绿茶"},
@@ -12,14 +12,24 @@ Vue.createApp({
                 {src_img:"../image/tea_kind3.jpg",img_text:"白茶"},
                 {src_img:"../image/tea_kind6.jpg",img_text:"黄茶"},
             ],
-            word_itme:[
-                {href:"",content:"铁观音",content:"龙井",content:"白茶",content:"碧螺春",}
+            word_items:[
+                {href:"",tea_content:["铁观音","龙井","白茶","碧螺春"]},
+                {href:"",tea_content:["铁观音","龙井","白茶","碧螺春"]},
+                {href:"",tea_content:["铁观音","龙井","白茶","碧螺春"]},
+                {href:"",tea_content:["铁观音","龙井","白茶","碧螺春"]},
+                {href:"",tea_content:["铁观音","龙井","白茶","碧螺春"]},
+                {href:"",tea_content:["铁观音","龙井","白茶","碧螺春"]}
+                // {href:"",tea_content:"铁观音",tea_content1:"龙井",tea_content2:"白茶",tea_content3:"碧螺春"}
             ]
         }
     },
     methods: {
-        seenter:function(){
-            
+        seenter:function(index){
+            this.judge_switch[index]=false;
+        },
+        leaver:function(index){
+            this.judge_switch[index]=true;
+        }
         }
 }
-}).mount('#tea_kind_id')
+).mount('#tea_kind_id')
