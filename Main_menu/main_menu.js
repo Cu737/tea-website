5db279ed72521a75315e637fcaed859d3e8c834e
liuchanghan <2594651085@qq.com>
2023-05-10 13:34:27 +0800

    vue修改

--- Main_menu/main_menu.js
+++ Main_menu/main_menu.js
@@ -3,6 +3,7 @@ Vue.createApp({
         return {
             text: "hello",
             message: "helloo",
+            judge_switch:true,
             facade_item:[
                 {src_img:"../image/tea_kind2.jpg",img_text:"红茶"},
                 {src_img:"../image/tea_kind1.jpg",img_text:"绿茶"},
@@ -10,7 +11,15 @@ Vue.createApp({
                 {src_img:"../image/tea_kind5.jpg",img_text:"普洱茶"},
                 {src_img:"../image/tea_kind3.jpg",img_text:"白茶"},
                 {src_img:"../image/tea_kind6.jpg",img_text:"黄茶"},
+            ],
+            word_itme:[
+                {href:"",content:"铁观音",content:"龙井",content:"白茶",content:"碧螺春",}
             ]
         }
-    }
+    },
+    methods: {
+        seenter:function(){
+            
+        }
+}
 }).mount('#tea_kind_id')
