5aa5a4f298168c1757b3fcac4d9520eb77afb1c4
雨后天晴 <oncwnuALQzkrUxUWw7Va9cRzwQVo@git.weixin.qq.com>
2023-05-17 16:24:18 +0800

    js功能更新

--- Solution1/gooddetail.js
+++ Solution1/gooddetail.js
@@ -4,6 +4,8 @@ let str2;
 let name;
 let tele;
 let addrss;
+let num;
+let cost;
 function add(){
     str = document.getElementById("text");
     str.value = Number(str.value) + 1;
@@ -12,14 +14,15 @@ function add(){
 }
 function sub() {
     str = document.getElementById("text");
-    str.value = str.value - 1;
+    str.value = Number(str.value) - 1;
     str2 = document.getElementById("cost");
     str2.value = Number(str.value) *120;
 }
 function check() {
-    str = document.getElementById("text");
-    str2 = document.getElementById("cost");
-    name = document.getElementById("name");
-    tele = document.getElementById("telenum");
-    address = document.getElementById("address");
+    num = document.getElementById("text").value;
+    cost = document.getElementById("cost").value;
+    name = document.getElementById("name").value;
+    tele = document.getElementById("telenum").value;
+    address = document.getElementById("address").value;
+    //ртио╣д
 }
